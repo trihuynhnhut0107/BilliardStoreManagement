e660696217db7375cd9ef041271155395c85e183
Tri Huynh <22521519@gm.uit.edu.vn>
2025-01-03 14:54:09 +0700

    create bill from booking

---
+++ Front-end/Store-side/components/BookingTable.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="w-full border-collapse border-none">
+    <table class="w-full border-collapse">
+      <!-- Table Header -->
+      <thead class="bg-white border-b border-gray-300">
+        <tr>
+          <th v-for="column in columns" :key="column.key" class="p-2">
+            {{ column.label }}
+          </th>
+          <th v-show="canChange" class="p-2">Actions</th>
+        </tr>
+      </thead>
+
+      <!-- Table Rows -->
+      <tbody>
+        <template v-for="(item, index) in filteredDataList" :key="item.id">
+          <!-- Display Row -->
+          <tr
+            class="hover:bg-gray-200 bg-white"
+            :class="{
+              'bg-blue-50 border-blue-300 w-[80px]':
+                selectedRow?.id === item.id,
+            }"
+            @dblclick="viewRow(item)">
+            <td
+              v-for="(value, key) in Object.entries(item).filter(
+                ([key]) => key !== 'createdAt' && key !== 'updatedAt'
+              )"
+              :key="key"
+              class="p-2 text-center align-middle w-[80px]">
+              {{
+                isNaN(value[1])
+                  ? toUpperCase(value[1])
+                  : Intl.NumberFormat("de-DE").format(value[1])
+              }}
+            </td>
+            <td class="p-2 w-[80px] text-center">
+              <button
+                :disabled="
+                  item.status == 'completed' || item.status == 'cancelled'
+                "
+                @click="bookingCheckout(item.id)"
+                class="py-[6px] px-4 bg-blue-500 text-white rounded"
+                :class="`${
+                  item.status == 'completed' || item.status == 'cancelled'
+                    ? 'opacity-50 cursor-not-allowed'
+                    : ''
+                }`">
+                Checkout
+              </button>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+
+    <!-- Pagination Controls -->
+    <div
+      class="flex justify-center items-center mt-4 gap-2 w-full py-2 px-10 rounded bg-white">
+      <button
+        @click="props.handlePageChange('prev')"
+        :class="{ 'opacity-50 cursor-not-allowed': props.currentPage === 1 }"
+        class="px-3 py-2 rounded">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          class="w-6 h-6"
+          viewBox="0 0 16 16">
+          <path
+            fill-rule="evenodd"
+            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+        </svg>
+      </button>
+      <span class="mx-2"
+        >Page {{ props.currentPage }} of {{ props.totalPages }}</span
+      >
+      <button
+        @click="props.handlePageChange('next')"
+        :class="{
+          'opacity-50 cursor-not-allowed':
+            props.currentPage === props.totalPages,
+        }"
+        class="px-3 py-2 rounded">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          class="w-6 h-6"
+          viewBox="0 0 16 16">
+          <path
+            fill-rule="evenodd"
+            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const dataList = defineModel();
+console.log(dataList.value);
+
+const selectedRow = ref(null);
+
+const emit = defineEmits(["bookingCheckout"]);
+
+const toUpperCase = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+// Extract column keys from the first row of the dataList (excluding 'createdAt' and 'updatedAt')
+const columns = computed(() => {
+  if (!dataList.value.length) return [];
+  return Object.keys(dataList.value[0])
+    .filter((key) => key !== "createdAt" && key !== "updatedAt")
+    .map((key) => ({
+      key,
+      label: key
+        .split("_")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" "),
+    }));
+});
+
+// Compute filtered data list
+const filteredDataList = computed(() => {
+  if (!dataList.value.length) return [];
+  const keysToMatch = Object.keys(dataList.value[0]);
+  return dataList.value.filter((item) =>
+    keysToMatch.every((key) => key in item)
+  );
+});
+
+// Define props for pagination
+const props = defineProps({
+  currentPage: {
+    type: Number,
+    required: true,
+  },
+  totalPages: {
+    type: Number,
+    required: true,
+  },
+  handlePageChange: {
+    type: Function,
+    required: true,
+  },
+  canChange: {
+    type: Boolean,
+    default: true,
+  },
+  canDelete: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const bookingCheckout = async (id) => {
+  emit("bookingCheckout", id);
+};
+</script>
